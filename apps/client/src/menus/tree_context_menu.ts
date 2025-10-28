66e499a2e14ea3cd798f25b8248c2a9ec4fed494
Jakob Schlanstedt <schlanstedtjakob@gmail.com>
2025-10-28 18:26:54 +0100

    refactor(note-create): replace enum with optional fields

--- apps/client/src/menus/tree_context_menu.ts
+++ apps/client/src/menus/tree_context_menu.ts
@@ -2,7 +2,7 @@ import NoteColorPicker from "./custom-items/NoteColorPicker.jsx";
 import treeService from "../services/tree.js";
 import froca from "../services/froca.js";
 import clipboard from "../services/clipboard.js";
-import noteCreateService, { CreateNoteAfterUrlOpts, CreateNoteIntoUrlOpts, CreateNoteTarget } from "../services/note_create.js";
+import noteCreateService, { CreateNoteAfterUrlOpts, CreateNoteIntoUrlOpts } from "../services/note_create.js";
 import contextMenu, { type MenuCommandItem, type MenuItem } from "./context_menu.js";
 import appContext, { type ContextMenuCommandData, type FilteredCommandNames } from "../components/app_context.js";
 import noteTypesService from "../services/note_types.js";
@@ -286,7 +286,7 @@ export default class TreeContextMenu implements SelectMenuItemEventListener<Tree
 
             noteCreateService.createNote(
                 {
-                    target: CreateNoteTarget.AfterNoteURL,
+                    target: "after",
                     parentNoteUrl: parentNotePath,
                     targetBranchId: this.node.data.branchId,
                     type: type,
@@ -300,7 +300,7 @@ export default class TreeContextMenu implements SelectMenuItemEventListener<Tree
 
             noteCreateService.createNote(
                 {
-                    target: CreateNoteTarget.IntoNoteURL,
+                    target: "into",
                     parentNoteUrl: parentNotePath,
                     type: type,
                     isProtected: this.node.data.isProtected,
