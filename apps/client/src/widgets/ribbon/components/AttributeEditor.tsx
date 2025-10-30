1ba903d63db0d8daa07076b63c3263a5ae9dab73
Jakob Schlanstedt <schlanstedtjakob@gmail.com>
2025-10-30 19:58:02 +0100

    fix(note_autocomplete): fix attributes not linking

--- apps/client/src/widgets/ribbon/components/AttributeEditor.tsx
+++ apps/client/src/widgets/ribbon/components/AttributeEditor.tsx
@@ -253,11 +253,6 @@ export default function AttributeEditor({ api, note, componentId, notePath, ntxI
             parentNotePath: string | undefined,
             action: CreateNoteAction
         ): Promise<string> => {
-            if (!parentNotePath) {
-                console.warn("Missing parentNotePath in createNoteFromCkEditor()");
-                return "";
-            }
-
             switch (action) {
                 case CreateNoteAction.CreateNoteIntoInbox:
                 case CreateNoteAction.CreateAndLinkNoteIntoInbox: {
@@ -265,7 +260,8 @@ export default function AttributeEditor({ api, note, componentId, notePath, ntxI
                         {
                             target: "inbox",
                             title,
-                            activate: false
+                            activate: false,
+                            promptForType: true,
                         }
                     );
                     return note?.getBestNotePathString() ?? "";
@@ -273,6 +269,10 @@ export default function AttributeEditor({ api, note, componentId, notePath, ntxI
 
                 case CreateNoteAction.CreateNoteIntoPath:
                 case CreateNoteAction.CreateAndLinkNoteIntoPath: {
+                    if (!parentNotePath) {
+                        console.warn("Missing parentNotePath in createNoteFromCkEditor()");
+                        return "";
+                    }
                     const resp = await note_create.createNote(
                         {
                             target: "into",
