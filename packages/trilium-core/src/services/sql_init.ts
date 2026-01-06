7c16aeca4a11b645da58ca7dc6fa6ffc825bf774
Elian Doran <contact@eliandoran.me>
2026-01-06 16:30:45 +0200

    chore(core): crash due to dbReady before CLS init

--- packages/trilium-core/src/services/sql_init.ts
+++ packages/trilium-core/src/services/sql_init.ts
@@ -2,4 +2,7 @@ import { deferred } from "@triliumnext/commons";
 
 export const dbReady = deferred<void>();
 
-dbReady.resolve();
+// TODO: Proper impl.
+setTimeout(() => {
+    dbReady.resolve();
+}, 850);
