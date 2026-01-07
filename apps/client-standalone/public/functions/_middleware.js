8b6da981f7df9d64adb18c189d2dcfcc4b35b9da
Elian Doran <contact@eliandoran.me>
2026-01-07 17:48:11 +0200

    chore(client-standalone): try to use plain header file

--- apps/client-standalone/public/functions/_middleware.js
+++
@@ -1,6 +0,0 @@
-export const onRequest = async (context) => {
-    const response = await context.next();
-    response.headers.set("Cross-Origin-Opener-Policy", "same-origin");
-    response.headers.set("Cross-Origin-Embedder-Policy", "require-corp");
-    return response;
-};
