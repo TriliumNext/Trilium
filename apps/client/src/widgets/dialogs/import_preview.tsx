47b53335af7941cec2c32dde8f134c4226ef26db
Elian Doran <contact@eliandoran.me>
2026-02-07 21:59:02 +0200

    feat(client/import_preview): display badges for

--- apps/client/src/widgets/dialogs/import_preview.tsx
+++ apps/client/src/widgets/dialogs/import_preview.tsx
@@ -1,9 +1,10 @@
 import "./import_preview.css";
 
-import { ImportPreviewResponse } from "@triliumnext/commons";
+import { DangerousAttributeCategory, ImportPreviewResponse } from "@triliumnext/commons";
 import { useState } from "preact/hooks";
 
 import { t } from "../../services/i18n";
+import { Badge } from "../react/Badge";
 import Button from "../react/Button";
 import { Card } from "../react/Card";
 import FormRadioGroup from "../react/FormRadioGroup";
@@ -14,10 +15,50 @@ export interface ImportPreviewData {
     previews: ImportPreviewResponse[];
 }
 
+const DANGEROUS_CATEGORIES_MAPPINGS: Record<DangerousAttributeCategory, {
+    title: string;
+    description: string;
+    icon: string;
+    category: "critical" | "warning";
+}> = {
+    clientSideScripting: {
+        icon: "bx bx-window-alt",
+        title: t("import_preview.badge_client_side_scripting_title"),
+        description: t("import_preview.badge_client_side_scripting_tooltip"),
+        category: "critical"
+    },
+    serverSideScripting: {
+        icon: "bx bx-server",
+        title: t("import_preview.badge_server_side_scripting_title"),
+        description: t("import_preview.badge_server_side_scripting_tooltip"),
+        category: "critical"
+    },
+    codeExecution: {
+        icon: "bx bx-terminal",
+        title: t("import_preview.badge_code_execution_title"),
+        description: t("import_preview.badge_code_execution_description"),
+        category: "critical"
+    },
+    iconPack: {
+        icon: "bx bx-package",
+        title: t("import_preview.badge_icon_pack_title"),
+        description: t("import_preview.badge_icon_pack_description"),
+        category: "warning"
+    },
+    webview: {
+        icon: "bx bx-globe",
+        title: t("import_preview.badge_web_view_title"),
+        description: t("import_preview.badge_web_view_description"),
+        category: "warning"
+    }
+};
+
 export default function ImportPreviewDialog() {
     const [ data, setData ] = useState<ImportPreviewData | null>({
         previews: [
-            JSON.parse(`{"isDangerous":true,"dangerousAttributes":["iconPack"],"dangerousAttributeCategories":["iconPack"],"numNotes":1,"id":"llpCPOmcBGhW5.trilium"}`)
+            JSON.parse(`{"isDangerous":true,"dangerousAttributes":["iconPack"],"dangerousAttributeCategories":["codeExecution", "serverSideScripting", "clientSideScripting"],"numNotes":1,"id":"llpCPOmcBGhW5.trilium"}`),
+            JSON.parse(`{"isDangerous":true,"dangerousAttributes":["iconPack"],"dangerousAttributeCategories":["codeExecution", "iconPack", "webview"],"numNotes":1,"id":"llpCPOmcBGhW5.trilium"}`),
+            JSON.parse(`{"isDangerous":false,"dangerousAttributes":["iconPack"],"dangerousAttributeCategories":[],"numNotes":1,"id":"llpCPOmcBGhW5.trilium"}`)
         ]
     });
     const [ shown, setShown ] = useState(true);
@@ -65,6 +106,30 @@ function SinglePreview({ preview }: { preview: ImportPreviewResponse }) {
     return (
         <Card title={preview.id}>
             <span>{t("import_preview.notes_count", { count: preview.numNotes })}</span>
+
+            <div className="dangerous-categories">
+                {preview.dangerousAttributeCategories.length > 1
+                    ? preview.dangerousAttributeCategories.map(dangerousCategory => {
+                        const mapping = DANGEROUS_CATEGORIES_MAPPINGS[dangerousCategory];
+                        return (
+                            <Badge
+                                key={dangerousCategory}
+                                className={mapping.category}
+                                icon={mapping.icon}
+                                text={mapping.title}
+                                tooltip={mapping.description}
+                            />
+                        );
+                    })
+                    : (
+                        <Badge
+                            className="safe"
+                            icon="bx bx-check"
+                            text="Safe"
+                            tooltip="This archive has no active content such as scripts or widgets that could affect your knowledge base or access sensitive data."
+                        />
+                    )}
+            </div>
         </Card>
     );
 }
