f6f7baf3923372cffb04be72895cb1f34652c8b4
BeatLink <git@beatlink.simplelogin.com>
2026-03-03 07:47:39 -0500

    Alphabetize attribute names

--- packages/commons/src/lib/attribute_names.ts
+++ packages/commons/src/lib/attribute_names.ts
@@ -48,11 +48,11 @@ type Labels = {
     geolocation: string;
     expanded: string;
     "calendar:hideWeekends": boolean;
-    "calendar:weekNumbers": boolean;
+    "calendar:initialDate": string;
     "calendar:slotDuration": string;
     "calendar:slotLabelInterval": string;
+    "calendar:weekNumbers": boolean;
     "calendar:view": string;
-    "calendar:initialDate": string;
     "map:style": string;
     "map:scale": boolean;
     "map:hideLabels": boolean;
