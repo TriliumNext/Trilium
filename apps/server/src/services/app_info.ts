e4ffe7c3edfc0da8c22012c1e7ce421e5d24c911
contributor <foss.contributor@gmail.com>
2026-04-14 13:05:45 +0300

    fix incorrect app DB version number

--- apps/server/src/services/app_info.ts
+++ apps/server/src/services/app_info.ts
@@ -5,7 +5,7 @@ import packageJson from "../../package.json" with { type: "json" };
 import build from "./build.js";
 import dataDir from "./data_dir.js";
 
-const APP_DB_VERSION = 236;
+const APP_DB_VERSION = 237;
 const SYNC_VERSION = 38;
 const CLIPPER_PROTOCOL_VERSION = "1.0";
 
